8d3297dca2874ac5635223c017b4927be93029a1
chanducodemonk <chanducodemonk@gmail.com>
2018-11-07 16:03:28 +0530

    adding relation between in dataset by creating two sqldatadapters and loading data in to datagrid as olden days and adding relation by using single sqlDataAdapter.

---
+++ DBOperations/Form10.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace DBOperations
+{
+    public partial class Form10 : Form
+    {
+        string Constr;
+        SqlDataAdapter da;
+        DataSet ds;
+        SqlConnection con;
+        DataRelation dr;
+
+        public Form10()
+        {
+            InitializeComponent();
+        }
+
+        private void Form10_Load(object sender, EventArgs e)
+        {
+            
+            Constr = ConfigurationManager.ConnectionStrings["SConstr"].ConnectionString;
+            ds = new DataSet();
+            con = new SqlConnection(Constr);
+            da = new SqlDataAdapter("select *from dept",con);
+            da.Fill(ds, "Dept");
+            da.SelectCommand.CommandText = "select *from emp";
+            da.SelectCommand.Connection = con;
+            da.Fill(ds, "Emp");
+            dr = new DataRelation("DeptEmp",ds.Tables[0].Columns["Deptno"],ds.Tables[1].Columns["Deptno"]);
+            ds.Relations.Add(dr);
+            dataGrid1.DataSource=ds;
+        }
+    }
+}
---
+++ DBOperations/Form9.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DBOperations
+{
+    public partial class Form9 : Form
+    {
+        DataSet ds;
+        SqlDataAdapter da1,da2;
+        SqlConnection con;
+        DataRelation dr;
+        string Constr;
+
+        public Form9()
+        {
+            InitializeComponent();
+        }
+
+        private void Form9_Load(object sender, EventArgs e)
+        {
+            Constr = ConfigurationManager.ConnectionStrings["SConstr"].ConnectionString;
+            con = new SqlConnection(Constr);
+            ds = new DataSet();
+            da1 = new SqlDataAdapter("select * from dept",con);
+            da2 = new SqlDataAdapter("select *from emp", con);
+            da1.Fill(ds, "Dept");
+            da2.Fill(ds, "Emp");
+            da2 = new SqlDataAdapter("select *from Emp", con);
+            dr = new DataRelation("EmpDept",ds.Tables[0].Columns["Deptno"],ds.Tables[1].Columns["Deptno"]);
+            ds.Relations.Add(dr);
+            dr.ChildKeyConstraint.DeleteRule = Rule.None;
+            dr.ChildKeyConstraint.UpdateRule = Rule.None;
+            dataGridView1.DataSource = ds.Tables[0];
+            dataGridView2.DataSource = ds.Tables[1];
+        }
+    }
+}
